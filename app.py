45da7f29c0af81c7d900cb099d5f3838f507f5a9
Tax-Technology <138007036+Tax-Technology@users.noreply.github.com>
2023-08-09 01:31:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+import streamlit as st
+import xmlschema
+import requests
+
 def get_element_info(element, level=0):
     info = f"{'  ' * level}- {element.name} (Type: {element.type_name})"
     for sub_element in element.type.iter_elements():
         info += "\n" + get_element_info(sub_element, level + 1)
     return info
 
+def get_keyref_info(schema):
+    keyref_info = []
+    for element_name, element in schema.elements.items():
+        if element.keyref:
+            keyref_info.extend([f"- Element: {element_name}, Keyref: {keyref.name}" for keyref in element.keyref])
+    return keyref_info
+
 def main():
+    # Import the st module
+    import streamlit as st
+
     st.title("XSD-to-XML Generator and Schema Documentation")
 
     # Dropdown options for predefined XSD files
@@ -29,14 +43,12 @@ def main():
                 for root_element_name in xsd.root_elements:
                     root_element = xsd.elements[root_element_name]
                     st.write(f"Root Element: {root_element_name} (Type: {root_element.type_name})")
-                    info = get_element_info(root_element)
-                    st.write(info)
+                    st.write(get_element_info(root_element))
 
             keyref_info = get_keyref_info(xsd)
             if keyref_info:
                 st.write("Keyref Elements:")
-                for keyref in keyref_info:
-                    st.write(str(keyref))
+                st.write(keyref_info)
 
             # Generate the XML document.
             generated_xml = xsd.tostring()
